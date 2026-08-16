a09fa6d81e45423a5dcc266a7aee8802438bec2a
Paras Pawar <pwparas0@gmail.com>
2026-08-16 23:18:16 +0530

    Time: 39 ms (83.67%) | Memory: 13.3 MB (57.05%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 public:
     int numSquares(int n) {
 
@@ -28,5 +28,27 @@ public:
         }
 
         return 3;
+    }
+}; */
+
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1, n + 1);
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j * j <= i; j++) {
+
+                int square = j * j;
+
+                dp[i] = min(dp[i],
+                             dp[i - square] + 1);
+            }
+        }
+
+        return dp[n];
     }
 };
