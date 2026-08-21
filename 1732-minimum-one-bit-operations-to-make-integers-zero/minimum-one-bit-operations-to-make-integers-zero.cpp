fcf8b1507f8d8288310efe99cc647013032c73d3
Paras Pawar <pwparas0@gmail.com>
2026-08-22 00:09:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (58.99%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 public:
     int minimumOneBitOperations(int n) {
         int ans = 0;
@@ -8,5 +8,31 @@ public:
         }
 
         return ans;
+    }
+}; */
+
+class Solution {
+public:
+
+    int solve(int n) {
+
+        if (n == 0)
+            return 0;
+
+        int k = 0;
+
+        while ((1 << k) <= n) {
+            k++;
+        }
+
+        k--;
+
+        int highest = (1 << k);
+
+        return (2 * highest - 1) - solve(n - highest);
+    }
+
+    int minimumOneBitOperations(int n) {
+        return solve(n);
     }
 };
