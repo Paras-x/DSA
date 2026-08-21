ea81a5c682769691dc8215da1b9f2510c58d57a6
Paras Pawar <pwparas0@gmail.com>
2026-08-21 22:39:28 +0530

    Time: 3 ms (45.22%) | Memory: 10.7 MB (15.07%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 public:
     char kthCharacter(int k) {
 
@@ -12,7 +12,33 @@ public:
             word += temp;
         }
 
-        
         return word[k - 1];
     }
+}; */
+
+class Solution {
+public:
+    void generate(string &word, int k) {
+        if (word.length() >= k) {
+            return;
+        }
+
+        string temp = "";
+        for (char ch : word) {
+
+            temp += ch + 1;
+        }
+
+        word += temp;
+
+        generate(word, k);
+    }
+    char kthCharacter(int k) {
+        string word = "a";
+
+        generate(word, k);
+
+        return word[k - 1];
+    }
+
 };
