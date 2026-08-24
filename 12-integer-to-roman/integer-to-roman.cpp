f66331832adc8777cbb35b9d708f1fae8e44b3c5
Paras Pawar <pwparas0@gmail.com>
2026-08-24 23:47:07 +0530

    Time: 2 ms (71.57%) | Memory: 8.2 MB (95.47%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 public:
     string intToRoman(int num) {
 
@@ -22,6 +22,90 @@ public:
                 ans += symbol[i];
                 num -= value[i];
             }
+        }
+
+        return ans;
+    }
+}; */
+
+class Solution {
+public:
+    string intToRoman(int num) {
+
+        string ans = "";
+
+        // Thousands
+        int d = num / 1000;
+        for (int i = 0; i < d; i++) {
+            ans += "M";
+        }
+        num = num % 1000;
+
+
+        // Hundreds
+        d = num / 100;
+
+        if (d <= 3) {
+            for (int i = 0; i < d; i++)
+                ans += "C";
+        }
+        else if (d == 4) {
+            ans += "CD";
+        }
+        else if (d <= 8) {
+            ans += "D";
+
+            for (int i = 0; i < d - 5; i++)
+                ans += "C";
+        }
+        else { // d == 9
+            ans += "CM";
+        }
+
+        num = num % 100;
+
+
+        // Tens
+        d = num / 10;
+
+        if (d <= 3) {
+            for (int i = 0; i < d; i++)
+                ans += "X";
+        }
+        else if (d == 4) {
+            ans += "XL";
+        }
+        else if (d <= 8) {
+            ans += "L";
+
+            for (int i = 0; i < d - 5; i++)
+                ans += "X";
+        }
+        else { // d == 9
+            ans += "XC";
+        }
+
+        num = num % 10;
+
+
+        // Ones
+        d = num;
+
+        if (d <= 3) {
+            for (int i = 0; i < d; i++)
+                ans += "I";
+        }
+        else if (d == 4) {
+            ans += "IV";
+        }
+        else if (d <= 8) {
+            ans += "V";
+
+            for (int i = 0; i < d - 5; i++)
+                ans += "I";
+        }
+        else { // d == 9
+            ans += "IX";
         }
 
         return ans;
