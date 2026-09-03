582db7a9968b80c0d196edcb319b19c254312be4
Paras Pawar <pwparas0@gmail.com>
2026-09-03 23:53:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (44.56%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+
+        stack<int> st;
+        st.push(0);
+
+        for (char c : s) {
+
+            if (c == '(') {
+                st.push(0);
+            }
+            else {
+                int x = st.top();
+                st.pop();
+
+                int score = (x == 0) ? 1 : 2 * x;
+
+                st.top() += score;
+            }
+        }
+
+        return st.top();
+    }
+};
