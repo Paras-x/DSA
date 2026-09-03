a748962f921d531ccc794fa0759f7c85b88e91f9
Paras Pawar <pwparas0@gmail.com>
2026-09-03 23:50:27 +0530

    Time: 17 ms (5.81%) | Memory: 16.6 MB (5.09%) - LeetSync

--- 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.cpp
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 public:
     int minLengthAfterRemovals(string s) {
 
@@ -15,5 +15,40 @@ public:
         }
 
         return abs(a - b);
+    }
+}; */
+
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+
+        stack<pair<char, int>> st;
+
+        for(char c : s) {
+
+            if(st.empty()) {
+                st.push({c, 1});
+            }
+            else if(st.top().first == c) {
+                st.top().second++;
+            }
+            else {
+                // a aur b different hain -> ek pair remove
+                st.top().second--;
+
+                if(st.top().second == 0) {
+                    st.pop();
+                }
+            }
+        }
+
+        int ans = 0;
+
+        while(!st.empty()) {
+            ans += st.top().second;
+            st.pop();
+        }
+
+        return ans;
     }
 };
