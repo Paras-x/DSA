aaffecb62171e5b1aac496b49ad17c49c3d90851
Paras Pawar <pwparas0@gmail.com>
2026-09-03 12:36:46 +0530

    Time: 32 ms (56.49%) | Memory: 101.6 MB (5.00%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,5 +1,30 @@
 class StockSpanner {
 public:
+    stack<int> st;
+    vector<int> price;
+    vector<int> ans;
+
+    int next(int p) {
+
+        int i = price.size();
+
+        price.push_back(p);
+        ans.push_back(1);
+
+        while (!st.empty() && price[st.top()] <= price[i]) {
+
+            ans[i] += ans[st.top()];
+
+            st.pop();
+        }
+
+        st.push(i);
+
+        return ans[i];
+    }
+};
+/* class StockSpanner {
+public:
     stack<pair<int, int>> st;
 
     StockSpanner() {
@@ -17,4 +42,28 @@ public:
 
         return span;
     }
-};
+}; */
+
+/* class StockSpanner {
+public:
+    stack<int> st;
+    vector<int> ans(n, 1);
+
+    for(int i = n -1; i >= 0; i--){
+        while(!ist.empty() && price[i] > price[st.top()]){
+            ans[st.top()] = st.top() - i;
+            st.pop();
+        }
+
+        st.push(i);
+
+        while(!is.empty()){
+            ans[st.top()] = st.top() + 1;
+            st.pop();
+        }
+
+        return st;
+    }
+
+}; */
+
