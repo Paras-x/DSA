b90da15be10c48cd6de9eb2112ec4246f5632dc7
Paras Pawar <pwparas0@gmail.com>
2026-09-03 23:50:17 +0530

    Time: 2 ms (72.82%) | Memory: 14.9 MB (44.91%) - LeetSync

---
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+
+        int a = 0;
+        int b = 0;
+
+        for(char c : s) {
+            if(c == 'a') {
+                a++;
+            }
+            else {
+                b++;
+            }
+        }
+
+        return abs(a - b);
+    }
+};
