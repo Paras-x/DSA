cd84dd0d73dcb067bd4d12ef0bb06400aa5e690e
Paras Pawar <pwparas0@gmail.com>
2026-09-06 20:37:10 +0530

    Time: 2 ms (70.46%) | Memory: 56.7 MB (11.20%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -1,4 +1,4 @@
-var twoSum = function(nums, target) {
+/* var twoSum = function(nums, target) {
 
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
@@ -10,4 +10,32 @@ var twoSum = function(nums, target) {
         }
     }
 
+}; */
+
+var twoSum = function(nums, target) {
+
+    let arr = nums.map((value, index) => [value, index]);
+
+    arr.sort((a, b) => a[0] - b[0]);
+
+    let i = 0;
+    let j = arr.length - 1;
+
+    while (i < j) {
+
+        let sum = arr[i][0] + arr[j][0];
+
+        if (sum === target) {
+            return [arr[i][1], arr[j][1]];
+        }
+        else if (sum < target) {
+            i++;
+        }
+        else {
+            j--;
+        }
+    }
+
+    return [];
 };
+
