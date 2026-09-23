c7d6784acd989974819a398fa1fa78f3f31b9ad5
Paras Pawar <pwparas0@gmail.com>
2026-09-23 23:29:44 +0530

    Time: 9 ms (38.49%) | Memory: 33 MB (61.78%) - LeetSync

---
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,29 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int k;
+
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+
+        for (int num : nums) {
+            pq.push(num);
+
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+    }
+
+    int add(int val) {
+    
+        if (pq.size() < k) {
+            pq.push(val);
+        }else if( val > pq.top()){
+            pq.pop();
+            pq.push(val);
+        }
+
+        return pq.top();
+    }
+};
